48ec0520914b90e81570b89b9eaf4ea7ba1bcc2c
Artur Metelskiy <artur.metelskiy@gmail.com>
2022-05-15 03:16:36 +0300

    feat: add header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import AboutUsPageComponent from './components/about-us-page/about-us-page.component';
+import CotactsPageComponent from './components/cotacts-page/cotacts-page.component';
+import GalleryPageComponent from './components/gallery-page/gallery-page.component';
+import MainComponent from './components/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'contacts', component: CotactsPageComponent },
+  { path: 'gallery', component: GalleryPageComponent },
+  { path: 'about-us', component: AboutUsPageComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
